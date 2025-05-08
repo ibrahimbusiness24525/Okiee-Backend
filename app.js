eac9b35ba3facc3368a918e10986693d9e4f74ac
arshmanahmad <arshmanmuhammad153@gmail.com>
2025-05-08 11:38:18 +0500

    update

--- app.js
+++ app.js
@@ -24,12 +24,12 @@ const app = express();
 connectDB();
 app.use(cors({ origin: "*", credentials: true }));
 
-// app.use(cors());
-// app.use(cors({
-//     origin: 'https://www.okiiee.com', 
-//     methods: ['GET', 'POST', 'PUT', 'DELETE'], 
-//     allowedHeaders: ['Content-Type', 'Authorization'], 
-//   }));
+app.use(cors());
+app.use(cors({
+    origin: 'https://www.okiiee.com', 
+    methods: ['GET', 'POST', 'PUT', 'DELETE'], 
+    allowedHeaders: ['Content-Type', 'Authorization'], 
+  }));
 app.use(express.json());
 app.use(express.json({ limit: "50mb" }));  // Increase payload limit
 app.use(express.urlencoded({ extended: true, limit: "50mb" }));
